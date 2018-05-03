02ffc94b50555e08304351d96f9f4da57308a9bf
Naomi Schalken <ducoveen@gmail.com>
2018-05-03 13:44:17 +0200

    extra output

--- DAC_uniform.R
+++ DAC_uniform.R
@@ -154,9 +154,9 @@ DAC.uniform <- function(from, to, by, data, priors, lb.bench, ub.bench, n.iter=1
       KL.experts[i,2] <- KL.experts[i,1]/KL.bench
     }
     
-    out <- list(output.data = output.data, KL.DAC = KL.experts)
+    out <- list(output.data = output.data, KL.DAC = KL.experts, KL.bench = KL.bench)
     
-    print(KL.experts)
+    print(list(KL.experts=KL.experts, KL.bench = KL.bench))
     
     # Return output
     return(out)
